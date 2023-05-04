c78f85ed64e5451ab4b854e475b4fb2a68217f74
unknown <salvajs@uma.es>
2023-05-04 22:42:31 +0200

    Mejorado algoritmo de seguimiento del helicoptero a la guia agente. Incluye frenos en caso de que se pase de la velocidad del agente, por lo tanto, va a su misma velocidad. Además, frena a una distancia determinada antes de llegar al objetivo.

--- Proyecto 2/Assets/Scripts/HelicopteroScript.cs
+++ Proyecto 2/Assets/Scripts/HelicopteroScript.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class HelicopteroScript : MonoBehaviour
 {
     private enum Estado {DESPEGAR, SEGUIRGUIA}
     private Estado estado;
     private const float VELVERT = 0.2f;
-    private const float VELHOR = 10f;
+    private const float VELHOR = 100f;
     private float alturaDeseada;
     private Rigidbody rb;
     private float masa;
@@ -85,21 +86,51 @@ public class HelicopteroScript : MonoBehaviour
         // Posición objeto teniendo en cuenta la altura del helicóptero.
         Vector3 posObj = new Vector3(objeto.transform.position.x, transform.position.y, objeto.transform.position.z);
         Vector3 vectorDireccionObjetivo = posObj - transform.position;
-        float velRel = objeto.GetComponent<Rigidbody>().velocity.magnitude - rb.velocity.magnitude;
+        //float velRel = objeto.GetComponent<Rigidbody>().velocity.magnitude - rb.velocity.magnitude;
         float anguloVDirYVVel = Vector3.Angle(vectorDireccionObjetivo, rb.velocity);
-        if(velRel > 0 || anguloVDirYVVel < 70)
+        float velocidadGuia = objeto.GetComponent<NavMeshAgent>().velocity.magnitude;
+        
+        if (anguloVDirYVVel > 70) // Si el ángulo entre el vector dirección al objetivo y el vector velocidad del helicoptero
+                                  // es mayor de 70, es decir, nos hemos pasado del objetivo.
+        {
+            if (Vector3.Distance(transform.position, posObj) >= 3) // Si la distancia es mayor a 3, debemos girarnos para volver
+                                                                   // atrás, ya que nos hemos pasado demasiado.
+            {
+                transform.LookAt(posObj);
+                rb.AddForce(transform.forward * masa * 10);
+                print("Me paso con distancia mayor a 3.");
+            }
+            else    // Si la distancia es menor a 3, no hace falta girar. Corregimos para llegar al objetivo.
+            {
+                rb.AddForce(vectorDireccionObjetivo * masa * 10);
+                print("Me paso con distancia menor a 3.");
+            }
+            rb.AddForce(-rb.velocity * 5 * masa); // Debemos frenar para corregir.
+        }
+        else if (Vector3.Distance(transform.position, posObj) < 3) // Si la distancia entre el helicoptero y el objetivo es
+                                                                   // menor a 3, mantenemos posición.
         {
             float factor = vectorDireccionObjetivo.magnitude * velocidadHorizontal;
             rb.AddForce(vectorDireccionObjetivo * factor);
-            //transform.LookAt(posObj);
-        }else if(velRel <= 0)
-        {
-            rb.AddForce(-rb.velocity * 5 * masa); // fallo
+            print("Mantener posición.");
         }
-        else
+        else if(Vector3.Distance(transform.position, posObj) < rb.velocity.magnitude
+                || (velocidadGuia > 0 && rb.velocity.magnitude > velocidadGuia)) // Frenamos en caso de que vayamos más rapido
+                                                                                 // que el objeto guia agente que seguimos,
+                                                                                 // o si la distancia entre el objeto guia
+                                                                                 // agente y el helicoptero es menor que la
+                                                                                 // velocidad del helicoptero.
         {
-            rb.AddForce(vectorDireccionObjetivo * 10 * masa);
-            rb.AddForce(-rb.velocity * 5 * masa);
+            //rb.AddForce(-transform.forward * masa);
+            rb.AddForce(-rb.velocity * masa);
+            print("Frenar.");
+        }
+        else            // En cualquier otro caso aceleramos hacia el objeto guia.
+        {
+            transform.LookAt(posObj);
+            float factor = vectorDireccionObjetivo.magnitude * velocidadHorizontal;
+            rb.AddForce(transform.forward * factor);
+            print("Acelerar.");
         }
     }
 }
