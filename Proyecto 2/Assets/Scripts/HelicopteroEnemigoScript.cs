57fcf06957fad8d3ead8349ffc0fa1ad3e70909d
juanroman1313 <juanroman1313@uma.es>
2023-05-11 15:52:07 +0200

    Cambiado el AlcanzarPosicion del helicoptero enemigo

--- Proyecto 2/Assets/Scripts/HelicopteroEnemigoScript.cs
+++ Proyecto 2/Assets/Scripts/HelicopteroEnemigoScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using static UnityEngine.GraphicsBuffer;
 
 public class HelicopteroEnemigoScript : MonoBehaviour
@@ -8,17 +9,19 @@ public class HelicopteroEnemigoScript : MonoBehaviour
     public enum Estado {DESPEGAR,VAGAR,ESQUIVAR }
     public Estado estado;
     private const float VELVERT = 0.2f;
+    private const float VELGIR = 1000f;
+    public const float VELHOR = 100f;
     private float alturaDeseada;
     private Rigidbody rb;
     private float masa;
     private float fuerzaLevitacion;
     private RaycastHit[] detectSens;
     private Vector3 posicionDeseada;
-    public float RapidezHorizontal = 10f;
     public GameObject guia;
 
     private bool edificioObstaculo;
     private float tiempoSubida;
+    private bool giroDerecha;
     void Start()
     {
         estado = Estado.DESPEGAR;
@@ -30,6 +33,7 @@ public class HelicopteroEnemigoScript : MonoBehaviour
         posicionDeseada = transform.position;
         edificioObstaculo = false;
         tiempoSubida = 0f;
+        giroDerecha = false;
     }
     private void FixedUpdate()
     {
@@ -59,7 +63,7 @@ public class HelicopteroEnemigoScript : MonoBehaviour
     private void Vagar()
     {
         AlcanzarAltura(alturaDeseada, VELVERT);
-        AlcanzarPosicion(guia, RapidezHorizontal);
+        AlcanzarPosicion(guia, VELHOR);
         if (Vector3.Distance(detectSens[0].point, guia.transform.position)<2)
         {
             guia.GetComponent<GuiaEnemigoScript>().CambiarDestino();
@@ -95,8 +99,8 @@ public class HelicopteroEnemigoScript : MonoBehaviour
     {
         while (true)
         {
-            alturaDeseada += 0.5f;
-            yield return new WaitForSeconds(0.1f);
+            alturaDeseada += 0.3f;
+            yield return new WaitForSeconds(0.5f);
         }
     }
     private void AlcanzarAltura(float altura, float velocidadVertical)
@@ -118,20 +122,81 @@ public class HelicopteroEnemigoScript : MonoBehaviour
         // Posición objeto teniendo en cuenta la altura del helicóptero.
         Vector3 posObj = new Vector3(objeto.transform.position.x, transform.position.y, objeto.transform.position.z);
         Vector3 vectorDireccionObjetivo = posObj - transform.position;
-        float velRel = objeto.GetComponent<Rigidbody>().velocity.magnitude - rb.velocity.magnitude;
+        //float velRel = objeto.GetComponent<Rigidbody>().velocity.magnitude - rb.velocity.magnitude;
         float anguloVDirYVVel = Vector3.Angle(vectorDireccionObjetivo, rb.velocity);
-        if (velRel > 0 || anguloVDirYVVel < 70)
+        float velocidadGuia = objeto.GetComponent<NavMeshAgent>().velocity.magnitude;
+
+        if (anguloVDirYVVel > 70) // Si el ángulo entre el vector dirección al objetivo y el vector velocidad del helicoptero
+                                  // es mayor de 70, es decir, nos hemos pasado del objetivo.
+        {
+            if (Vector3.Distance(transform.position, posObj) >= 3) // Si la distancia es mayor a 3, debemos girarnos para volver
+                                                                   // atrás, ya que nos hemos pasado demasiado.
+            {
+                //transform.LookAt(posObj);
+                giroFisico(vectorDireccionObjetivo, VELGIR);
+                rb.AddForce(transform.forward * masa * 10);
+                print("Me paso con distancia mayor a 3.");
+            }
+            else    // Si la distancia es menor a 3, no hace falta girar. Corregimos para llegar al objetivo.
+            {
+                rb.AddForce(vectorDireccionObjetivo * masa * 10);
+                print("Me paso con distancia menor a 3.");
+            }
+            rb.AddForce(-rb.velocity * 5 * masa); // Debemos frenar para corregir.
+        }
+        else if (Vector3.Distance(transform.position, posObj) < 3) // Si la distancia entre el helicoptero y el objetivo es
+                                                                   // menor a 3, mantenemos posición.
         {
             float factor = vectorDireccionObjetivo.magnitude * velocidadHorizontal;
             rb.AddForce(vectorDireccionObjetivo * factor);
-            rb.transform.LookAt(posObj);
+            print("Mantener posición.");
+        }
+        else if (Vector3.Distance(transform.position, posObj) < rb.velocity.magnitude
+                || (velocidadGuia > 0 && rb.velocity.magnitude > velocidadGuia)) // Frenamos en caso de que vayamos más rapido
+                                                                                 // que el objeto guia agente que seguimos,
+                                                                                 // o si la distancia entre el objeto guia
+                                                                                 // agente y el helicoptero es menor que la
+                                                                                 // velocidad del helicoptero.
+        {
+            //rb.AddForce(-transform.forward * masa);
+            rb.AddForce(-rb.velocity * masa);
+            print("Frenar.");
+        }
+        else            // En cualquier otro caso aceleramos hacia el objeto guia.
+        {
+            //transform.LookAt(posObj);
+            giroFisico(vectorDireccionObjetivo, VELGIR);
+            float factor = vectorDireccionObjetivo.magnitude * velocidadHorizontal;
+            rb.AddForce(transform.forward * factor);
+            print("Acelerar.");
+        }
+    }
+    private void giroFisico(Vector3 vectorDir, float velocidadGiro)
+    {
+
+        Vector3 perpYObjPos = Vector3.Cross(Vector3.up, vectorDir);
+        Vector3 perpYObjNeg = -Vector3.Cross(Vector3.up, vectorDir);
+        float anguloPos = Vector3.Angle(perpYObjPos, transform.forward);
+        float anguloNeg = Vector3.Angle(perpYObjNeg, transform.forward);
+        Vector3 ejeGiro = Vector3.up;
+        if (anguloPos < anguloNeg)
+        {
+            ejeGiro *= -1;
+            if (giroDerecha)
+            {
+                rb.AddRelativeTorque(ejeGiro * velocidadGiro * 50);
+                giroDerecha = false;
+            }
         }
         else
         {
-            rb.transform.LookAt(posObj);
-            rb.AddForce(transform.forward * masa * 10);
-            rb.AddForce(-rb.velocity* 5 * masa);
+            if (!giroDerecha)
+            {
+                rb.AddRelativeTorque(ejeGiro * velocidadGiro * 50);
+                giroDerecha = true;
+            }
         }
+        rb.AddRelativeTorque(ejeGiro * velocidadGiro);
     }
     // Sensores para detectar objetos del entorno.
     private bool Sensores()
